c7752fae78f37e9eb41b33c84335cd362fca3915
Saeed <hassanisaeed19@yahoo.com>
2022-11-10 18:40:18 +0330

    feat: retrieve data from text fields

--- main.py
+++ main.py
@@ -1,89 +1,172 @@
-from PyQt6.QtCore import QDateTime, Qt, QTimer
-from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
-        QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
-        QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy, QVBoxLayout,
-        QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit, QWidget)
-import subprocess
- 
- 
-     
-
-def run_camera():
-    from collections import namedtuple
-    from observer import ConcreteSubject, WebSocketObserver, FFMpegObserver 
-
-    subject = ConcreteSubject()
-    
-    WebSocket = namedtuple('WebSocket','relay_js_path secret port_in port_out')
-    web_socket = WebSocket(
-                 relay_js_path= '/home/saeed/Desktop/camera/websocket-relay.js',
-                 secret= 'supersecret', 
-                 port_in= 8081,
-                 port_out= 8082,)
-    
-    observer_ws = WebSocketObserver(web_socket)
-    
-    subject.attach(observer_ws)
-    
-    Camera = namedtuple('Camera', 'username password ip port size rate')
-    camera = Camera('admin', '123456', '192.168.1.120', 554, '640x480', '1000k')
-    
-    Socket = namedtuple('Socket', 'host port secret')
-    socket = Socket('http://localhost',
-                     8081, 
-                    'supersecret')
-    
-    observer_ffmpeg = FFMpegObserver(camera, socket)
-    
-    subject.attach(observer_ffmpeg)
-
-    subject.run_camera()
-
-
+from PyQt6.QtWidgets import (QApplication, QCheckBox, QDialog, QGridLayout, QGroupBox,
+                             QLabel, QLineEdit, QPushButton, QStyleFactory)
     
 class WidgetGallery(QDialog):
+    def run_camera(self): 
+        from collections import namedtuple
+        from manager import ConcreteSubject, WebSocketObserver, FFMpegObserver 
+        self.ws_socket = {
+                 'relay_js_path': self.txtRelayJsPath.text(),
+                 'secret': self.txtSecret.text(), 
+                 'port_in': int(self.txtPortIn.text()),
+                 'port_out': int(self.txtPortOut.text()),
+        }
+        
+        self.camera = {
+            'ffmpeg': self.txtFFMpegPath.text(),
+            'username': self.txtUsername.text(),
+             'password': self.txtPassword.text(),
+             'ip': self.txtIP.text(),
+             'port': int(self.txtPort.text()),
+             'size': self.txtSize.text(),
+             'rate': self.txtRate.text(),
+        }
+        
+        self.socket = {
+            'host': self.txtHost.text(),
+            'port': int(self.txtPortIn.text()),
+            'secret': self.txtSecret.text()
+        }
+        
+        subject = ConcreteSubject()
+        
+        WebSocket = namedtuple('WebSocket','relay_js_path secret port_in port_out')
+        web_socket = WebSocket(**self.ws_socket)
+        
+        self.observer_ws = WebSocketObserver(web_socket)
+        
+        subject.attach(self.observer_ws)
+        
+        Camera = namedtuple('Camera', 'ffmpeg username password ip port size rate')
+        camera = Camera(**self.camera)
+        
+        Socket = namedtuple('Socket', 'host port secret')
+        socket = Socket(**self.socket)
+        
+        self.observer_ffmpeg = FFMpegObserver(camera, socket)
+        
+        subject.attach(self.observer_ffmpeg)
+
+        subject.run_camera()
+
+        
+
+    
+    def check_status(self):
+        try:
+            message = '%s - %s' % (self.observer_ffmpeg.is_running()[1], self.observer_ws.is_running()[1])
+            color = self.observer_ffmpeg.is_running()[0]
+            self.lblActive.setText(message)
+            self.lblActive.setStyleSheet(color)
+        except AttributeError:
+            pass
+    
     def __init__(self, parent=None):
         super(WidgetGallery, self).__init__(parent)
 
         self.originalPalette = QApplication.palette()
+        self.txtFFMpegPath = QLineEdit('ffmpeg')
+        self.txtUsername = QLineEdit('admin')
+        self.txtPassword = QLineEdit('123456')
+        self.txtIP = QLineEdit('192.168.1.120')
+        self.txtPort = QLineEdit('554')
+        self.txtSize = QLineEdit('640x480')
+        self.txtRate = QLineEdit('1000k')
+        self.txtHost = QLineEdit('http://localhost')
+        
+        self.txtRelayJsPath = QLineEdit('/home/saeed/Desktop/camera/websocket-relay.js')
+        self.txtPortIn = QLineEdit('8081')
+        self.txtPortOut = QLineEdit('8082')
+        self.txtSecret = QLineEdit('supersecret')
 
-        styleComboBox = QComboBox()
-        styleComboBox.addItems(QStyleFactory.keys())
+        self.camera_settings()  
+        self.websocket_settings()  
 
-        styleLabel = QLabel("برای فعالسازی دوربین بر روی دکمه زیر کلیک کنید") 
- 
-        self.createTopRightGroupBox()  
+        self.topLayout = QGridLayout()
+        btnRunCamera = QPushButton('روشن | ON', self)
+        
+        btnCameraStatus = QPushButton('بررسی وضعیت | Status', self)
 
-        topLayout = QHBoxLayout()
-        topLayout.addWidget(styleLabel) 
+        self.topLayout.addWidget(btnRunCamera, 0, 2) 
+        self.topLayout.addWidget(btnCameraStatus, 0, 1) 
+          
+        self.lblActive = QLabel('دوربین خاموش است') 
+        self.lblActive.setStyleSheet('color: red')
+        
+        self.topLayout.addWidget(self.lblActive, 0, 0)
+        
+        chkDisableSettings = QCheckBox("&Disable Settings")
+        chkDisableSettings.toggled.connect(self.topLeftGroupBox.setDisabled)
+        chkDisableSettings.toggled.connect(self.topRightGroupBox.setDisabled)
+        chkDisableSettings.setChecked(True)
         
         mainLayout = QGridLayout()
-        mainLayout.addLayout(topLayout, 0, 0, 1, 1) 
+        mainLayout.addWidget(chkDisableSettings)
+        mainLayout.addLayout(self.topLayout, 0, 1) 
+        mainLayout.addWidget(self.topLeftGroupBox, 1, 1)
         mainLayout.addWidget(self.topRightGroupBox, 1, 0)
+        
+        mainLayout.addWidget(QLabel('carvann.ir | سامانه کاروان'), 2, 0)
+        
         self.setLayout(mainLayout)
 
-        self.setWindowTitle("فعالسازی دوربین")
+        self.setWindowTitle("Camera | دوربین")
         QApplication.setStyle(QStyleFactory.create('Fusion'))
- 
+        
+        btnRunCamera.clicked.connect(self.run_camera)
+        btnCameraStatus.clicked.connect(self.check_status)
+        
 
-    def createTopRightGroupBox(self):
-        self.topRightGroupBox = QGroupBox() 
+    def camera_settings(self):
+        self.topLeftGroupBox = QGroupBox("Camera") 
+        self.txtPassword.setEchoMode(QLineEdit.EchoMode.Password) 
+        
+        layout = QGridLayout()
+        layout.addWidget(QLabel('Username: '), 0, 0) 
+        layout.addWidget(self.txtUsername, 0, 1)  
+        
+        layout.addWidget(QLabel('Password: ')) 
+        layout.addWidget(self.txtPassword)  
+         
+        layout.addWidget(QLabel('IP: ')) 
+        layout.addWidget(self.txtIP)  
+         
+        layout.addWidget(QLabel('Port: ')) 
+        layout.addWidget(self.txtPort)  
+         
+        layout.addWidget(QLabel('Size: ')) 
+        layout.addWidget(self.txtSize)  
+         
+        layout.addWidget(QLabel('Rate: ')) 
+        layout.addWidget(self.txtRate) 
+        
+        self.topLeftGroupBox.setLayout(layout)        
+        
+        
+    def websocket_settings(self):
+        self.topRightGroupBox = QGroupBox("Socket")
+         
+        self.txtSecret.setEchoMode(QLineEdit.EchoMode.Password)
+        
+        layout = QGridLayout()
+        layout.addWidget(QLabel('Host: '), 0, 0) 
+        layout.addWidget(self.txtHost, 0, 1)  
+        
+        layout.addWidget(QLabel('Port(in): ')) 
+        layout.addWidget(self.txtPortIn)  
+         
+        layout.addWidget(QLabel('Port(out): ')) 
+        layout.addWidget(self.txtPortOut)  
+         
+        layout.addWidget(QLabel('Secret: ')) 
+        layout.addWidget(self.txtSecret)
+         
+        layout.addWidget(QLabel('Relay Path: ')) 
+        layout.addWidget(self.txtRelayJsPath)
+        
+        self.topRightGroupBox.setLayout(layout)        
+         
 
-        togglePushButton = QPushButton("روشن کردن دوربین")
-        togglePushButton.setCheckable(True)
-        togglePushButton.setChecked(True)
-        togglePushButton.clicked.connect(run_camera)
-
-        nom_plan_label = QLabel()
-        nom_plan_label.setText('دوربین روشن است')
-        nom_plan_label.setObjectName('nom_plan_label')
-        nom_plan_label.setStyleSheet('QLabel#nom_plan_label {color: green}')
-
-        layout = QVBoxLayout() 
-        layout.addWidget(togglePushButton) 
-        layout.addWidget(nom_plan_label) 
-        layout.addStretch(1)
-        self.topRightGroupBox.setLayout(layout)
  
 if __name__ == '__main__':
 
